d58f7b343476818e0f92a61b67550ef37b73713a
Timothy Choe <timothychoe@MacBook-Pro.local>
2020-06-18 20:02:39 -0400

    Updates

---
+++ precache-manifest.7c841c30bfa58e406beb5453ff7c287b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ef0bf51fcf1f9beefd558e3d06c24f",
+    "url": "/Coding-bootcamp-testimonials-slider/index.html"
+  },
+  {
+    "revision": "a5515d7102d5c39a769b",
+    "url": "/Coding-bootcamp-testimonials-slider/static/css/main.4c48667a.chunk.css"
+  },
+  {
+    "revision": "26d605e505a7d2eb3b35",
+    "url": "/Coding-bootcamp-testimonials-slider/static/js/2.c9094c52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Coding-bootcamp-testimonials-slider/static/js/2.c9094c52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5515d7102d5c39a769b",
+    "url": "/Coding-bootcamp-testimonials-slider/static/js/main.7348481d.chunk.js"
+  },
+  {
+    "revision": "ba4c83b98c263466d730",
+    "url": "/Coding-bootcamp-testimonials-slider/static/js/runtime-main.4b17be5d.js"
+  },
+  {
+    "revision": "5421c364274a5e7333553851fc6b3714",
+    "url": "/Coding-bootcamp-testimonials-slider/static/media/icon-next.5421c364.svg"
+  },
+  {
+    "revision": "0fa4e8f2cf4223608fc878a70b8357af",
+    "url": "/Coding-bootcamp-testimonials-slider/static/media/icon-prev.0fa4e8f2.svg"
+  },
+  {
+    "revision": "9c1595060977e272ff1bea10380bfa6c",
+    "url": "/Coding-bootcamp-testimonials-slider/static/media/image-john.9c159506.jpg"
+  },
+  {
+    "revision": "343537c13a0f40dadbc3cca8d5ff30d1",
+    "url": "/Coding-bootcamp-testimonials-slider/static/media/image-tanya.343537c1.jpg"
+  },
+  {
+    "revision": "bb83e5bd104d93b264cae1bdfe037a4e",
+    "url": "/Coding-bootcamp-testimonials-slider/static/media/pattern-bg.bb83e5bd.svg"
+  },
+  {
+    "revision": "ae8c06393898e366f54e5fd28a9b3ae2",
+    "url": "/Coding-bootcamp-testimonials-slider/static/media/pattern-curve.ae8c0639.svg"
+  },
+  {
+    "revision": "83ba950c9ccf56bd0b8871f5007e0908",
+    "url": "/Coding-bootcamp-testimonials-slider/static/media/pattern-quotes.83ba950c.svg"
+  }
+]);
